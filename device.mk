6a7cde30e1adb63f9d4b81cb66365431d24296d0
Murat Kozan <muratkozan350@gmail.com>
2020-10-19 00:10:29 +0300

    kuntao: Switch to Sound trigger HAL V2.3

--- device.mk
+++ device.mk
@@ -57,7 +57,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
